791b39b541122f0440ba715e589142645e05b5c8
yasminalmeidadasilva <rm86396@estudante.fieb.edu.br>
2023-05-08 11:51:13 -0300

    apaguei tudo

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // laço enqunto- while
-        int contador = 0;
-        while (contador < 10) {
-            // processos
-            System.out.println("Contador = " + contador);
-            contador = contador + 1; //Incremento
-        }
-        // laço faça enquanto- do while
-        int cont = 1;
-        do {
-            System.out.println(cont);
-            cont++; //Incremento
-        } while (cont <= 10);
+        // Aula inicial de Orientação á Objetos
 
-        // laço para faça for
-        for (int x = 0; x < 10; x++) {
-            System.out.println(x);
-        }
     }
-    }
+}
